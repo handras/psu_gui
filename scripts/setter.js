cfc15c8882984d22a7fe929c87a181f570858621
handras <horvathandras159@gmail.com>
2020-07-26 15:22:41 +0200

    add data-togle buttons for turn on/off

--- scripts/setter.js
+++ scripts/setter.js
@@ -1,11 +1,13 @@
-function setChannel(channel){
+function setChannel(channel, turn_on){
     if (channel == '1'){
         data = $('#ch1').serialize()
+        data += "&ch1_state="+turn_on
     }
     if (channel == '2'){
         data = $('#ch2').serialize()
+        data += "&ch2_state="+turn_on
     }
-    console.log("Setting channel "+ channel + ' to ' + data);
+    console.log("Setting channel "+ channel + ' to ' + turn_on + " with " + data);
     $.ajax({
       method: "POST",
       url: "set.json",
